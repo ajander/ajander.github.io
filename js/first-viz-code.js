004b994ccda2b06c95a5508bdb05788eda7a052a
ajander <adrienne.j.anderson@gmail.com>
2017-05-16 18:02:42 -0500

    Tried adding bar chart js to blog post

---
+++ js/first-viz-code.js
@@ -0,0 +1,15 @@
+// define input data
+var data = [4, 8, 15, 16, 23, 42];
+
+// Part I: Using just regular html elements
+
+var x = d3.scale.linear()
+	.domain([0, d3.max(data)])
+	.range([0, 420]);
+
+d3.select('.chart')
+  .selectAll('div')
+    .data(data)
+  .enter().append('div')
+  	.style('width', function(d) { return x(d) + 'px'; })
+  	.text(function(d) { return d; });
