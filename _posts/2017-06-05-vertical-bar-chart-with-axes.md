91ce4fabb3a9bd06562097d90e343fe3f7e5a6d5
ajander <adrienne.j.anderson@gmail.com>
2017-06-05 21:12:13 -0500

    added files for new post
    6/5/17 vertical bar chart with axes

---
+++ _posts/2017-06-05-vertical-bar-chart-with-axes.md
@@ -0,0 +1,35 @@
+---
+layout: post
+title: "Basic Bar Chart with Tooltips"
+date: 2017-05-21
+---
+
+<head>
+<style>
+
+	div.tooltip {	
+	    position: absolute;			
+	    text-align: center;			
+	    width: 80px;					
+	    height: 40px;					
+	    padding: 6px;				
+	    font: 12px "Arial";		
+	    background: white;
+	    border: 1px solid #c0c0c0;
+	    box-shadow: 0px 2px 2px #888888;
+	    pointer-events: none;			
+	}
+
+</style>
+</head>
+
+Sources:
+* Mike Bostock's [Absolute-Positioned Tooltip](https://bl.ocks.org/mbostock/1087001)
+* d3noob's [Simple d3.js tooltips](http://bl.ocks.org/d3noob/a22c42db65eb00d4e369)
+
+<body>
+	<svg class="bar-chart"></svg>
+	<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+	<script src="https://ajander.github.io/js/2017-05-21-bar-chart-with-tooltips.js"></script>
+
+</body>
---
+++ js/2017-06-05-vertical-bar-chart-with axes.js
@@ -0,0 +1,54 @@
+var width = 420,
+ 	barHeight = 20;
+
+var x = d3.scale.linear()
+	.range([0, 420]);
+
+var chart = d3.select('.bar-chart')
+	.attr('width', width)
+
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+function mousemove() {
+  div.style("left", (d3.event.pageX + 10) + "px")
+      .style("top", (d3.event.pageY - 58) + "px");
+}
+
+function mouseout() {
+  div.style("opacity", 0);
+}
+
+d3.tsv('https://ajander.github.io/data.tsv', type, function(error, data) {
+	x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+	chart.attr('height', barHeight * data.length);
+
+	var bar = chart.selectAll('g')
+		.data(data)
+	  .enter().append('g')
+	  	.attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; })  	
+	 	.on("mouseover", function(d, i) {
+		  	div.style("opacity", 1)
+		  		.html("<strong>Data</strong><hr/>" + d.name + ", "  + d.value);
+		})
+	    .on("mousemove", mousemove)
+	    .on("mouseout", mouseout);
+
+	bar.append('rect')
+	 	.attr('width', function(d) { return x(d.value); })
+	 	.attr('height', barHeight - 1);
+
+	bar.append('text')
+	 	.attr('x', function(d) { return x(d.value) - 3; })		// x position of text
+	 	.attr('y', barHeight / 2)						// y position of text, relative to g
+	 	.attr('dy', '.35em')							// center the text vertically
+	 	.text(function(d) { return d.value; });
+
+})
+
+function type(d) {
+	d.value = +d.value;	// coerce to number type
+	return d;
+}
